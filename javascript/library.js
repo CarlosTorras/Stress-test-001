272c2512630e1ee142719c7971f310e4f3c9297b
Carlos Torras <carlos6torras@gmail.com>
2019-09-10 11:41:08 +0200

    start of vue branch

--- javascript/library.js
+++ javascript/library.js
@@ -1,89 +1,48 @@
-// Fetch for books
+// start of Vue for bookcards library
 
-fetch("https://api.myjson.com/bins/zyv02", {
-  method: "GET",
-  headers: {}
-})
-  .then(function(books) {
-    console.log(books);
-    return books.json();
-  })
-  .then(function(books) {
-    console.log(books);
-    var books = books.books;
+var app = new Vue({
+  el: "#app",
+  data: {
+    loading: true,
+    books: [],
+    search: ""
+  },
+  methods: {
+    getData() {
+      fetch("https://api.myjson.com/bins/zyv02", {
+        method: "GET",
+        headers: {}
+      })
+        .then(function(library) {
+          console.log(library);
+          return library.json();
+        })
+        .then(function(library) {
+          console.log(library);
+          app.books = library.books;
+          app.loading = false;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  },
+  computed: {
+    getBooks() {
+      return this.books.filter(book => {
+        let titlefilter = book.title
+          .toUpperCase()
+          .includes(this.search.toUpperCase());
+        let descriptionfilter = book.description
+          .toUpperCase()
+          .includes(this.search.toUpperCase());
 
-    bookcards(books, "library");
-    document.getElementById("forsearch").addEventListener("keyup", function() {
-      bookSearch(books);
-    });
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
+        return titlefilter || descriptionfilter;
+      });
+    }
+  },
 
-function bookcards(array, id) {
-  var body = document.getElementById(id);
-
-  body.innerHTML = "";
-
-  for (let i = 0; i < array.length; i++) {
-    var flipcard = document.createElement("div");
-    flipcard.className = "flip-card";
-    var flipcardinner = document.createElement("div");
-    flipcardinner.setAttribute("class", "flip-card-inner");
-    var flipcardfront = document.createElement("div");
-    flipcardfront.setAttribute("class", "flip-card-front");
-    var cover = document.createElement("img");
-    cover.setAttribute("src", array[i].cover);
-    var flipcardback = document.createElement("div");
-    flipcardback.setAttribute("class", "flip-card-back");
-    var title = document.createElement("h2");
-    title.className = "title";
-    var info = document.createElement("p");
-    info.className = "info";
-    var btnfunction = document.createElement("a");
-    btnfunction.setAttribute("href", array[i].detail);
-    btnfunction.setAttribute("data-fancybox", "gallery");
-    btnfunction.setAttribute("data-caption", array[i].title);
-    var button = document.createElement("button");
-    button.setAttribute("type", "button");
-
-    title.innerHTML = array[i].title;
-    info.innerHTML =
-      array[i].description + "<br>" + "language:" + " " + array[i].language;
-    button.innerHTML = "More Info";
-
-    flipcardfront.append(cover);
-    btnfunction.append(button);
-    flipcardback.append(title, info, btnfunction);
-    flipcardinner.append(flipcardfront, flipcardback);
-
-    flipcard.append(flipcardinner);
-    body.append(flipcard);
+  created() {
+    this.getData();
   }
-
-  document.getElementById("load-icon").style.display = "none";
-}
-
-// Search input
-
-function bookSearch(array) {
-  var input = document.getElementById("forsearch");
-  var filteredArr = [];
-  let noresult = document.getElementById("noresults");
-  noresult.style.display = "none";
-
-  filteredArr = array.filter(
-    arr =>
-      arr.title.toUpperCase().includes(input.value.toUpperCase()) ||
-      arr.description.toUpperCase().includes(input.value.toUpperCase())
-  );
-
-  console.log(filteredArr);
-
-  bookcards(filteredArr, "library");
-
-  if (filteredArr == "") {
-    noresult.style.display = "block";
-  }
-}
+});
